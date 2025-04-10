52ffc5b6faabe8beeb634906add9048b45d66c90
oscarponce <ponceoscarj@gmail.com>
2025-04-10 15:28:05 +0100

    changes to make to input and output

--- debugging_file.py
+++ debugging_file.py
@@ -1,9 +1,25 @@
 import jiwer
 
-reference = "decision making"
-hypothesis = "decision-making"
+gold_standard = "non disabling"
+predicted = "nondisabling"
 
-print(jiwer.RemovePunctuation()(hypothesis))
+# nondisabling, non-disabling, non disabling
 
-error = jiwer.wer(reference, hypothesis)
+# print(jiwer.RemovePunctuation()(predicted))
+# Delete all punctuations. Delete anything in brackets [laughter]. [unintelligible]. - '
+# Output, is the output with I'm Im
+
+input
+# apostrophes are deleted
+# hyphens are deleted
+
+# Need to correct in gold standard
+# hyphens should be transferred into 1 space
+# consistent numbers
+
+# Check in models output afap 024
+# check pre-diabetic, non-disabling
+# nondisabling, non disabling, non-disabling
+
+error = jiwer.wer(gold_standard, predicted)
 print(error)
