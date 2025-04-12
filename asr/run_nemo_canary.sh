fa14f13a054d669bd38134fbb034c36f8b5a2462
Oscar Ponce <64445959+ponceoscarj@users.noreply.github.com>
2025-04-12 10:04:32 +0100

    Update run_nemo_canary.sh

--- asr/run_nemo_canary.sh
+++ asr/run_nemo_canary.sh
@@ -199,7 +199,7 @@ run_experiment() {
 
     {
         echo -e "\n=== Starting Transcription ===\n"
-        python3 /home/ext_alzahidy_misk_mayo_edu/speech_ker/asr/nemo_aed_chunked_infer.py \
+        python3 /home/ext_ponceponte_oscar_mayo_edu/speech_ker/asr/nemo_aed_chunked_infer.py \
             model_path="${model_path}" \
             pretrained_name="${pretrained_name}" \
             dataset_manifest="${dataset_manifest}" \
@@ -213,7 +213,7 @@ run_experiment() {
             }
 
         echo -e "\n=== Calculating WER ===\n"
-        python3 /home/ext_alzahidy_misk_mayo_edu/speech_ker/asr/new_wer_calculator.py \
+        python3 /home/ext_ponceponte_oscar_mayo_edu/speech_ker/asr/new_wer_calculator.py \
             -i "${output_file}" \
             -v || {
                 echo "ERROR: WER calculation failed for ${output_file}"
