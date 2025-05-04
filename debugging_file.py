032913a2859209f02ec24fdc450aeaa817a02096
oscarponce <ponceoscarj@gmail.com>
2025-05-04 19:23:08 +0100

    fix

--- debugging_file.py
+++ debugging_file.py
@@ -1,37 +1,8 @@
-import jiwer
-
-gold_standard = "non disabling"
-predicted = "nondisabling"
-
-# nondisabling, non-disabling, non disabling
-
-# print(jiwer.RemovePunctuation()(predicted))
-# Delete all punctuations. Delete anything in brackets [laughter]. [unintelligible]. - '
-# Output, is the output with I'm Im
-
-input
-# apostrophes are deleted
-# hyphens are deleted
-
-# Need to correct in gold standard
-# hyphens should be transferred into 1 space
-# consistent numbers
-
-# Check in models output afap 024
-# check pre-diabetic, non-disabling
-# nondisabling, non disabling, non-disabling
 
 
-# Decisions
-# anti coagulation to anticoagulation
 
-error = jiwer.wer(gold_standard, predicted)
-print(error)
+num1 = 3
+num2 = 4
 
-
-from asr.normalizer.normalizer import EnglishTextNormalizer
-normalizer = EnglishTextNormalizer()
-new_text = f'''tis the season'''
-
-print('\n',new_text,'\n')
-print('\n','normalized_text:\n', EnglishTextNormalizer()(new_text), '\n')
+print(3 + 4 )
+print(num1 + num2)
